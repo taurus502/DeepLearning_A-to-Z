67e19b5d2a8761e9c21aa0fe8fb1689e023b76cf
Daniel RYU <36293717+taurus502@users.noreply.github.com>
2018-02-19 08:54:02 +0900

    Create train_mnist.py

---
+++ train_mnist.py
@@ -0,0 +1,13 @@
+import numpy as np
+import tensorflow as tf
+import gzip
+import random
+
+NUM_TRAIN_DATA = 60000
+NUM_TEST_DATA = 10000
+NUM_EPOCH = 20
+NUM_BATCH = 500
+PROB = 0.7
+
+def read_data(train):
+  if train
