c79d816d0b16e8c22d1d5f8007f85d395e06fc22
Daniel RYU <36293717+taurus502@users.noreply.github.com>
2018-02-19 10:08:56 +0900

    Update mnist4offline.py

--- mnist4offline.py
+++ mnist4offline.py
@@ -9,53 +9,55 @@ NUM_EPOCH = 20
 NUM_BATCH = 500
 PROB = 0.7 #dropout
 
-#read MNIST gzip data format / output = image, label (nparray)
-def read_data(if_test): #if_test == 0 : train case / else : test case
+#read MNIST gzip data format / output : train image, label, test image, label (nparray)
+def read_data():
   PATH_TRAIN_IMG = '/home/ubuntu/mnist/train-images-idx3-ubyte.gz'
   PATH_TRAIN_LBL = '/home/ubuntu/mnist/train-labels-idx1-ubyte.gz'
   PATH_TEST_IMG = '/home/ubuntu/mnist/t10k-images-idx3-ubyte.gz' 
   PATH_TEST_LBL = '/home/ubuntu/mnist/t10k-labels-idx1-ubyte.gz'
   
-  #train case
-  if if_test == 0:  
-    fp_img = gzip.open(PATH_TRAIN_IMG, 'rb')
-    fp_lbl = gzip.open(PATH_TRAIN_LBL, 'rb')
-    fp_img.read(16) #not used data
-    fp_lbl.read(8) #not used data
+  ### MNIST train data set ###
+  fp_img1 = gzip.open(PATH_TRAIN_IMG, 'rb')
+  fp_lbl1 = gzip.open(PATH_TRAIN_LBL, 'rb')
+  fp_img1.read(16) #not used data
+  fp_lbl1.read(8) #not used data
     
-    train_img = np.zeros(shape=(NUM_TRAIN_DATA, 784)) # (60000, 784)
-    train_lbl = np.zeros(shape=(NUM_TRAIN_DATA, 10)) # (60000, 10)
+  train_img = np.zeros(shape=(NUM_TRAIN_DATA, 784)) # (60000, 784)
+  train_lbl = np.zeros(shape=(NUM_TRAIN_DATA, 10)) # (60000, 10)
     
-    for i in range(NUM_TRAIN_DATA):
-      img = np.squeeze(np.frombuffer(fp_img.read(784), np.uint8))
-      train_img[i] = img
-      
-      lbl = np.frombuffer(fp_lbl.read(1), np.uint8)
-      lbl_onehot = np.squeeze(np.eye(10)[lbl]) #number -> one-hot format // 2 ->[0,0,1,0,0,0...]
-      train_lbl[i] = lbl_onehot
-      
-    return train_img.astype(dtype = np.float32), train_lbl.astype(dtype = np.float32) #component format : uint8 -> float32
-  
-  #test case
-  else:
-    fp_img = gzip.open(PATH_TEST_IMG, 'rb')
-    fp_lbl = gzip.open(PATH_TEST_LBL, 'rb')
-    fp_img.read(16) #not used data
-    fp_lbl.read(8) #not used data
-    
-    test_img = np.zeros(shape=(NUM_TEST_DATA, 784)) # (10000, 784)
-    test_lbl = np.zeros(shape=(NUM_TEST_DATA, 10)) # (10000, 10)
-    
-    for i in range(NUM_TEST_DATA):
-      img = np.squeeze(np.frombuffer(fp_img.read(784), np.uint8))
-      test_img[i] = img
-      
-      lbl = np.frombuffer(fp_lbl.read(1), np.uint8)
-      lbl_onehot = np.squeeze(np.eye(10)[lbl]) #number -> one-hot format // 2 ->[0,0,1,0,0,0...]
-      test_lbl[i] = lbl_onehot
-      
-    return test_img.astype(dtype = np.float32), test_lbl.astype(dtype = np.float32) #component format : uint8 -> float32
+  for i in range(NUM_TRAIN_DATA):
+    img1 = np.squeeze(np.frombuffer(fp_img1.read(784), np.uint8))
+    train_img[i] = img1
 
+    lbl1 = np.frombuffer(fp_lbl1.read(1), np.uint8)
+    lbl_onehot1 = np.squeeze(np.eye(10)[lbl1]) #number -> one-hot format // 2 ->[0,0,1,0,0,0...]
+    train_lbl[i] = lbl_onehot1
+  
+  train_npimg = train_img.astype(dtype = np.float32)#component format : uint8 -> float32
+  train_nplbl = train_lbl.astype(dtype = np.float32)#component format : uint8 -> float32
+
+  ### MNIST test data set ###
+  fp_img2 = gzip.open(PATH_TEST_IMG, 'rb')
+  fp_lbl2 = gzip.open(PATH_TEST_LBL, 'rb')
+  fp_img2.read(16) #not used data
+  fp_lbl2.read(8) #not used data
+
+  test_img = np.zeros(shape=(NUM_TEST_DATA, 784)) # (10000, 784)
+  test_lbl = np.zeros(shape=(NUM_TEST_DATA, 10)) # (10000, 10)
+
+  for j in range(NUM_TEST_DATA):
+    img2 = np.squeeze(np.frombuffer(fp_img2.read(784), np.uint8))
+    test_img[j] = img2
+
+    lbl2 = np.frombuffer(fp_lbl2.read(1), np.uint8)
+    lbl_onehot2 = np.squeeze(np.eye(10)[lbl2]) #number -> one-hot format // 2 ->[0,0,1,0,0,0...]
+    test_lbl[j] = lbl_onehot2
+    
+  test_npimg = test_img.astype(dtype = np.float32)
+  test_nplbl = test_lbl.astype(dtype = np.float32)
+  
+  return train_npimg, train_nplbl, test_npimg, test_nplbl
+  
   
 #define neural network model
 def model(inputs, prob):#inputs : img / prob : prob. of dropout
@@ -83,16 +85,18 @@ y_ = tf.placeholder(tf.float32, [None, 10])
 hypothesis = model(x_, PROB)
 
 #cost function, optimization, saver
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = final, labels = y))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = hypothesis, labels = y_))
 opt = tf.train.AdamOptimizer(1e-3).minimize(loss)
 saver = tf.train.Saver()
 
+#load data
+train_x, train_y, test_x, test_y = read_data()
+
+#session
 with tf.Session() as sess:
   init = tf.global_variables_initializer()
   sess.run(init)
-  
-  xx, yy = read_data(0) #train case
-  
+    
   #SGD
   for j in range(NUM_EPOCH):
     avg_err = 0
@@ -105,4 +109,7 @@ with tf.Session() as sess:
       idx_end = idx_start + NUM_BATCH
       ltmp = idx[idx_start:idx_end]
       
-      img_shuffle = [
+      img_shuffle = [xx[k] for k in ltmp]
+      lbl_shuffle = [yy[k] for k in ltmp]
+      
+      
